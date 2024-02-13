10b44359bc4a6de26121e4fe4d2041acacb11b21
Trent Brew <tbrew1023@gmail.com>
2024-02-13 17:13:42 -0600

    fix order

--- src/routes/work2/+page.svelte
+++ src/routes/work2/+page.svelte
@@ -8,20 +8,24 @@
   })
 
   const images = [
-    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/sketch3_tinHSejyDe.png?token=',
-    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/green2_lHp2YmE4q3.png?token=',
+    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/17R95KgJXEm_9WbBvAOiHK.png?token=',
+    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/3WMNB5Od6U5_ZxqxoVEVxU.png?token=',
+    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/2XAW74SU1Ec_9ROc8sEzqu.png?token=',
+    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/4oIQ6emgZ2Y_IZPSuTvosR.png?token=',
     'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/image_76_P54hgI4IDy.png?token=',
     'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/image_78_EPKFbRzves.png?token=',
-    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/image_77_Sc4O3PyefE.png?token=',
+    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/bloom_KWjSH0PDNf.gif?token=',
+    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/sketch3_tinHSejyDe.png?token=',
+    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/green2_lHp2YmE4q3.png?token=',
     'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/tgi_NjI0NSRDlL.gif?token=',
     'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/speak2_LuASHwMJrg.gif?token=',
+    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/speak_USPSjOOjJA.gif?token=',
     'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/speak_USPSjOOjJA.gif?token=',
     'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/compressed_jEVD9tf6vV.png?token=',
     'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/screenshot_2024_01_05_at_6_16_itU6sO6i6h.png?token=',
     'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/image_67_DFeuRdEIIu.png?token=',
     'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/image_64_kP4XEFnNyw.png?token=',
     'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/last_RDgfoNaqu7.png?token=',
-    'https://trentbrew.pockethost.io/api/files/swvnum16u65or8w/ak09rb3bdh6bj3e/speak_USPSjOOjJA.gif?token=',
   ]
 </script>
 
