84747206fb88d62cf4ecfdca4e028db312fa3349
Trent Brew <tbrew1023@gmail.com>
2024-12-05 01:39:22 -0800

    oops

--- src/routes/embed/+page.svelte
+++ src/routes/embed/+page.svelte
@@ -43,7 +43,7 @@
   <meta name="description" content="Zakia Rowlett" />
 </svelte:head>
 
-<div class="invert">
+<div class="">
   <section id="_home" class="flex w-full h-screen justify-center items-center">
     <section
       id="hero"
